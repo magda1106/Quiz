f0958a64d4a786c3269661b7caf0ed6a24f5465f
unknown <magda11062000@o2.pl>
2021-12-11 18:31:54 +0100

    ready, with easy questions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const startButton = document.getElementById('start-button')
+const nextButton = document.getElementById('next-button')
 const questionContainerElement = document.getElementById('question-container')
 
 questionElement = document.getElementById('question')
@@ -8,7 +9,10 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
-
+nextButton.addEventListener('click',()=>{
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 
 
@@ -23,21 +27,96 @@ setNextQuestion()
 }
 
 function setNextQuestion(){
+    clearStatusClass(document.body)
+    resetState()
 showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 function showQuestion(question){
 questionElement.innerText = question.question
+question.answers.forEach(answer=>{
+    const button=document.createElement('button')
+    button.innerText=answer.text
+    button.classList.add('btn')
+
+    if(answer.correct){
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+})
+}
+function resetState(){
+nextButton.classList.add('hide')
+while(answerButtonsElement.firstChild){
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+}
 }
 
-function selectAnswer(){
+function selectAnswer(e){
+const selectedButton = e.target
+const correct = selectedButton.dataset.correct
+setStatusClass(document.body, correct)
+Array.from(answerButtonsElement.children).forEach(button =>{
+ setStatusClass(button, button.dataset.correct)
+})
+if(shuffledQuestions.length > currentQuestionIndex + 1){
+    nextButton.classList.remove('hide')
+}else{
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+}
+nextButton.classList.remove('hide')
+}
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if(correct){
+        element.classList.add('correct')
+    }else{
+        element.classList.add('wrong')
+    }
 
 }
+
+function clearStatusClass(element){
+element.classList.remove('correct')
+element.classList.remove('wrong')
+}
+
 const question=[
     {
-        question: 'What is 2+2 ? ',
+        question: 'What is 6+2 ? ',
         answers:[
-            {text: '4', correct: true },
-            {text:'22', correct:false}
+            {text: '8', correct: true },
+            {text:'22', correct:false},
+            {text:'23', correct:false},
+            {text:'242', correct:false}
+        ]
+    },
+    {
+            question: 'What is 10+2 ? ',
+            answers:[
+                {text: '5', correct: false },
+                {text:'12', correct:true},
+                {text:'23', correct:false},
+                {text:'234', correct:false}
+            ]
+    },
+    {
+        question: 'What is 10+2 ? ',
+        answers:[
+            {text: '5', correct: false },
+            {text:'12', correct:true},
+            {text:'23', correct:false},
+            {text:'234', correct:false}
+        ]
+    },
+    {
+        question: 'What is 14 -2 ? ',
+        answers:[
+            {text: '12', correct: true },
+            {text:'34', correct:false},
+            {text:'43', correct:false},
+            {text:'67', correct:false}
         ]
     }
 ]
